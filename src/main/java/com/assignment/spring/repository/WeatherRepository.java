d2c37baa07ef87d9648fb1026f4ed17aa6fb98f6
Marija Broceta <broceta.marija@gmail.com>
2021-04-06 09:43:13 +0200

    Added method to WeatherRepository

--- src/main/java/com/assignment/spring/repository/WeatherRepository.java
+++ src/main/java/com/assignment/spring/repository/WeatherRepository.java
@@ -2,8 +2,10 @@ package com.assignment.spring.repository;
 
 import com.assignment.spring.entity.WeatherEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WeatherRepository extends CrudRepository<WeatherEntity, Integer> {
 
-
+    WeatherEntity findByCity(String city);
 }
