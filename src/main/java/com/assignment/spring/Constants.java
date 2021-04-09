845ebca357249775fcd5c02bbaae3dd4fd68e51e
markoljubic10 <82096735+markoljubic10@users.noreply.github.com>
2021-04-09 13:47:13 +0200

    Update Constants.java
    Add apiID

--- src/main/java/com/assignment/spring/Constants.java
+++ src/main/java/com/assignment/spring/Constants.java
@@ -2,7 +2,7 @@ package com.assignment.spring;
 
 public class Constants {
 
-    public static final String APP_ID = "TBD";
+    public static final String APP_ID = "b1ff11fb495bac655130946f616b8d70";
 
     public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={appid}";
 }
